f5980e43569445989cd37e479fd41c4c372a06e1
AlexanderYekat <chemsaha@yandex.ru>
2024-02-24 10:14:02 +0500

    сделал парсинг pdf и перевод его в pdf

--- parcepdf.py
+++ parcepdf.py
@@ -7,107 +7,180 @@ import pdfplumber
 # Для извлечения изображений из PDF
 from PIL import Image
 from pdf2image import convert_from_path
-# Для выполнения OCR, чтобы извлекать тексты из изображений 
-import pytesseract 
 # Для удаления дополнительно созданных файлов
 import os
+from enum import Enum
+import csv
 #from pypdf import PdfReader
 
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+class ParseState(Enum):
+    Header = 1
+    Item = 2
+    Total = 3
+    Footer = 4
 
-# Создаём функцию для вырезания элементов изображений из PDF
-def crop_image(element, pageObj):
-    # Получаем координаты для вырезания изображения из PDF
-    [image_left, image_top, image_right, image_bottom] = [element.x0,element.y0,element.x1,element.y1] 
-    # Обрезаем страницу по координатам (left, bottom, right, top)
-    pageObj.mediabox.lower_left = (image_left, image_bottom)
-    pageObj.mediabox.upper_right = (image_right, image_top)
-    # Сохраняем обрезанную страницу в новый PDF
-    cropped_pdf_writer = PyPDF2.PdfWriter()
-    cropped_pdf_writer.add_page(pageObj)
-    # Сохраняем обрезанный PDF в новый файл
-    with open('cropped_image.pdf', 'wb') as cropped_pdf_file:
-        cropped_pdf_writer.write(cropped_pdf_file)        
+def pdftojson(pdf_path):
+    textofcheck = ""
+    with pdfplumber.open(pdf_path) as pdf:
+        for page in pdf.pages:
+            currStr = page.extract_text()
+            textofcheck = textofcheck + "\n"+currStr
+    return textofcheck
 
-# Создаём функцию для преобразования PDF в изображения
-def convert_to_images(input_file,):
-    images = convert_from_path(input_file)
-    image = images[0]
-    output_file = "PDF_image.png"
-    image.save(output_file, "PNG")
+directory = './inpdf/'
+checksfile = open("checks_header.csv", mode="w", encoding='utf-8')
+headernamesofchecks = ["fd", "fp", "type", "kassir", "summ", "nal", "beznal"]
+csv_writer_of_checks = csv.DictWriter(checksfile, delimiter = ";", lineterminator="\r", fieldnames=headernamesofchecks)
+csv_writer_of_checks.writeheader()
 
-# Создаём функцию для считывания текста из изображений
-def image_to_text(image_path):
-    # Считываем изображение
-    img = Image.open(image_path)
-    # Извлекаем текст из изображения
-    text = pytesseract.image_to_string(img)
-    return text
+postiotnsfile = open("checks_poss.csv", mode="w", encoding='utf-8')
+headernamespostiotns = ["fd", "name", "quant", "price", "nds", "psr", "ppr", "mark"]
+csv_writer_pos = csv.DictWriter(postiotnsfile, delimiter = ";", lineterminator="\r", fieldnames=headernamespostiotns)
+csv_writer_pos.writeheader()
 
-print("fdfdfdfd")
-#reader = PyPDF2.PdfReader("test3.pdf")
-reader = PyPDF2.PdfReader("marky.pdf")
-page = reader.pages[0]
-print(page.extract_text())
-meta = reader.metadata
-#print('author: %s'%meta.author)
-#print('title: %s'%meta.title)
-#print('creator: %s'%meta.creator)
-#print('subject: %s'%meta.subject)
-
-pdf_path =  "marky.pdf"
-for pagenum, page2 in enumerate(extract_pages(pdf_path)):
-    for element in page2:
-# Проверяем, является ли элемент текстовым
-        if isinstance(element, LTChar):
-            print("LTChar")
-            print(element.get_text())        
-            pass
-        if isinstance(element, LTTextContainer):
-            print("LTTextContainer")
-            # Функция для извлечения текста из текстового блока
-            pass
-            # Функция для извлечения формата текста
-            pass
-
-        # Проверка элементов на наличие изображений
-        if isinstance(element, LTFigure):
-            print("LTFigure")
-            crop_image(element, page)
-            #convert_to_images('cropped_image.pdf')
-            #image_text = image_to_text('PDF_image.png')
-            #print(image_text)
-            # Функция для преобразования PDF в изображение
-            pass
-            # Функция для извлечения текста при помощи OCR
-            pass
-
-        # Проверка элементов на наличие таблиц
-        if isinstance(element, LTRect):
-            print("LTRect")
-            # Функция для извлечения таблицы
-            pass
-            # Функция для преобразования содержимого таблицы в строку
-            pass    
-print("***************************")
-for element in page:
-    print(element)
-    if isinstance(element, LTChar):
-        print("LTChar")
-        print(element.get_text())
-    if isinstance(element, LTTextContainer):
-        print("LTTextContainer")
-        print(element.get_text())
-    if isinstance(element, LTFigure):
-        print("LTFigure")
-        print(element.name)
-    if isinstance(element, LTRect):
-        print("LTRect")
-        print(element.width)
-
-count = 0
-for image_file_object in page.images:
-    print(str(count) + image_file_object.name)
-    with open(str(count) + image_file_object.name, "wb") as fp:
-        fp.write(image_file_object.data)
-        count += 1
+currNumOfFile = 0
+listoffilesname = os.listdir(directory)
+amountoffiles = len(listoffilesname)
+for filename in listoffilesname:
+    if currNumOfFile % 100 == 0:
+        print("запись на диск...")
+        checksfile.flush()
+        postiotnsfile.flush()
+    currNumOfFile = currNumOfFile + 1
+    f = os.path.join(directory, filename)
+    print("обработка", currNumOfFile, "из", amountoffiles, "имя файла", f)
+    textofcheck = pdftojson(f)
+    listofstr = textofcheck.split("\n")
+    prevState = ParseState.Header
+    stateCurr = ParseState.Header
+    stateNew = ParseState.Header
+    currHeaderRow = {}
+    currPosRow = {}
+    rowsAll = []
+    currPos = 0
+    obrabBeginNewPos = False
+    prevWasPPR = False
+    prevWasNameKodTovara = False
+    prevWasKodTovara = False
+    currIsNameKodTovara = False
+    currIsKodTovara = False
+    currIsPPR = False
+    for str in listofstr:
+        stateCurr = stateNew
+        if len(str) == 0:
+            continue
+        obrabBeginNewPos = False
+        if stateCurr==ParseState.Header:
+            if str.find("Кассир") != -1:
+                stateNew = ParseState.Item
+        elif stateCurr == ParseState.Item:
+            if str.find("Итог") != -1:
+                stateCurr = ParseState.Total
+                stateNew = stateCurr
+        elif stateCurr == ParseState.Total:
+            if str.find("места расчетов") != -1:
+                stateCurr = ParseState.Footer
+                stateNew = stateCurr
+        if stateCurr == ParseState.Item:
+            currIsPPR = False
+            currIsNameKodTovara = False
+            currIsKodTovara = False            
+            if str[:11] == "Код товара:":
+                currIsNameKodTovara = True
+            if str[:3] == "ППР":
+                currIsPPR = True
+            if prevWasNameKodTovara:
+                currIsKodTovara = True                
+            #
+            if (prevWasKodTovara or prevWasPPR) and (not(currIsNameKodTovara)):
+                #новая позиция                                                     
+                obrabBeginNewPos = True
+            prevWasPPR = False
+            prevWasNameKodTovara = False
+            prevWasKodTovara = False
+            if currIsNameKodTovara:
+                prevWasNameKodTovara = True
+            if currIsKodTovara:
+               prevWasKodTovara = True 
+            if currIsPPR:
+                prevWasPPR = True
+        #обработка текущего состояния и строки текста для определения типа объекта и его данных
+        if stateCurr == ParseState.Header:
+            if str[:12] == "КАССОВЫЙ ЧЕК":
+                currHeaderRow.clear()
+                currHeaderRow.update({"type": "приход"})
+                if str.find("ВОЗВРАТ") != -1:
+                    currHeaderRow.update({"type": "возврат"})    
+            if str.find("Кассир") != -1:
+                strkassir = str.split("Кассир")[1].strip()
+                currHeaderRow.update({"kassir": strkassir})
+            #if str[:4] == "Дата":
+            #    sptempr = str.split(" ")
+            #    strdata = sptempr[1].strip()
+            #    strtime = sptempr[2].strip()
+            #    currHeaderRow.update({"date": strdata})
+            #    currHeaderRow.update({"time": strtime})
+        if prevState == ParseState.Header and stateCurr == ParseState.Item:
+            obrabBeginNewPos = True
+        if obrabBeginNewPos:
+            #обрабатывем конец позиции
+            if currPos != 0:
+                rowsAll.append(currPosRow.copy())
+            #новая позиция    
+            currPos=currPos+1
+            currPosRow.clear()
+            #currPosRow = {"name": str, "pos": currPos}
+            currPosRow = {"name": str}
+        if stateCurr == ParseState.Item:
+            if currIsKodTovara:
+                currPosRow.update({"mark": str.strip()})
+            if currIsPPR:
+                strPPR = str.split("ППР")[1].strip()
+                currPosRow.update({"ppr": strPPR})
+            if str[:10] == "Количество":
+                strquant = str.split("Количество")[1].strip()
+                currPosRow.update({"quant": strquant})
+            if str[:22] == "Цена с учетом скидок и":
+                strprice = str.split("Цена с учетом скидок и")[1].strip()
+                currPosRow.update({"price": strprice})
+            if str[:3] == "ПСР":
+                strPSR = str.split("ПСР")[1].strip()
+                currPosRow.update({"psr": strPSR})
+            if str[:10] == "Ставка НДС":
+                strNDS = str.split("Ставка НДС")[1].strip()
+                currPosRow.update({"nds": strNDS})
+        if stateCurr == ParseState.Total:
+            if str[:4] == "Итог":
+                strsumm = str.split("Итог")[1].strip()
+                currHeaderRow.update({"summ": strsumm})
+            if str[:9] == "Наличными":
+                strnal = str.split("Наличными")[1].strip()
+                currHeaderRow.update({"nal": strnal})
+            if str[:12] == "Безналичными":
+                strbeznal = str.split("Безналичными")[1].strip()
+                currHeaderRow.update({"beznal": strbeznal})   
+        if stateCurr == ParseState.Footer:
+            #if prevState!=ParseState.Footer:
+            #    print("**********Footer****************")
+            #if str[:2] == "ФН":
+            #    strfn = str.split("ФН")[1].strip()
+            #    currHeaderRow.update({"fn": strfn})
+            if str[:2] == "ФД":
+                strfd = str.split("ФД")[1].strip()
+                currHeaderRow.update({"fd": strfd})
+            if str[:3] == "ФПД":
+                strfp = str.split("ФПД")[1].strip()
+                currHeaderRow.update({"fp": strfp})
+            #if str[:3] == "СНО":
+            #    strsno = str.split("СНО")[1].strip()
+            #    currHeaderRow.update({"sno": strsno})
+            #if str[:6] == "РН ККТ":
+            #    strrn = str.split("РН ККТ")[1].strip()
+            #    currHeaderRow.update({"regnum": strrn})
+        prevState = stateCurr
+    if currPos != 0:
+        rowsAll.append(currPosRow.copy())
+    for currRowPos in rowsAll:
+        currRowPos["fd"] = currHeaderRow["fd"]
+    csv_writer_of_checks.writerow(currHeaderRow)
+    csv_writer_pos.writerows(rowsAll)
